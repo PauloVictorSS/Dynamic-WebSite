c9d01b60f5124d50ec04affffa018b303d18f090
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-07-30 19:52:36 -0300

    Finalização do CSS
    Finalização do CSS da página, junto com o @media

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang='pt-br'>
  <head>
     <title>Projeto 01</title>
-    <script src="https://kit.fontawesome.com/17abfde62c.js" crossorigin="anonymous"></script>
-    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link href="estilos/style.css" rel="stylesheet">
     <meta name="viewport" content="width-device-width, initial-scale-1.0">
     <meta name="description" content="Descrição do meu website">
@@ -23,6 +23,9 @@
                 </ul>
             </nav>
             <nav class="mobile right">
+                <div class="botao-menu-mobile">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </div>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre</a></li>
@@ -49,11 +52,11 @@
         <div class="center">
             <div class="w50 left">
                 <h2>Paulo Victor</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolore dicta reprehenderit quibusdam dignissimos, quis est atque, placeat alias consequatur perferendis unde autem! Cumque, doloribus voluptas sit obcaecati impedit beatae.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia inventore voluptas deserunt minima ducimus cum! Rerum nisi pariatur soluta voluptatibus laboriosam tempore cupiditate, doloremque quasi et! Veniam repellat tenetur aliquid.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolore dicta reprehenderit quibusdam dignissimos, quis est atque, placeat alias consequatur perferendis unde autem! Cumque, doloribus voluptas sit obcaecati impedit beatae.</p>
             </div>
             <div class="w50 left">
-                <img src="imagens/foto.jpg">
+                <img src="imagens/foto2.jpg" class="right">
             </div>
             <div class="clear"></div>
         </div>
@@ -63,18 +66,18 @@
         <div class="center">
             <h2 class="title">Especialidades</h2>
             <div class="w33 box-especialidade left">
-                <h3><i class="fab fa-css3"></i></h3>
-                <h3>CSS3</h3>
+                <h3><i class="fa fa-css3"></i></h3>
+                <h4>CSS3</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, hic. Excepturi sed amet, eaque sequi exercitationem itaque illo perferendis consectetur reprehenderit natus tenetur dolorum, iure rerum mollitia quo nihil impedit?</p>
             </div>
             <div class="w33 box-especialidade left">
-                <h3><i class="fab fa-html5"></i></h3>
-                <h3>HTML5</h3>
+                <h3><i class="fa fa-html5"></i></h3>
+                <h4>HTML5</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, hic. Excepturi sed amet, eaque sequi exercitationem itaque illo perferendis consectetur reprehenderit natus tenetur dolorum, iure rerum mollitia quo nihil impedit?</p>
             </div>
             <div class="w33 box-especialidade left">
-                <h3><i class="fas fa-code"></i></h3>
-                <h3>JavaScript</h3>
+                <h3><i class="fa fa-code"></i></h3>
+                <h4>JavaScript</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, hic. Excepturi sed amet, eaque sequi exercitationem itaque illo perferendis consectetur reprehenderit natus tenetur dolorum, iure rerum mollitia quo nihil impedit?</p>
             </div>
             <div class="clear"></div>
@@ -83,22 +86,22 @@
 
     <section class="extras">
         <div class="center">
-            <div class="w50 left">
-                <h2 class="title">Depoimentos</h2>
+            <div class="w50 left depoimentos-container">
+                <h2 class="title">Depoimentos dos nossos clientes</h2>
                 <div class="depoimento-single">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident pariatur omnis nam eius nihil, eaque, cumque totam velit aspernatur minus iure rem iusto laudantium illo ullam, reiciendis alias libero aliquam?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, ducimus rerum totam, voluptates nam hic sequi libero soluta saepe harum adipisci reprehenderit dolor incidunt quibusdam tempora tempore aut consequuntur cupiditate?</p>
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident pariatur omnis nam eius nihil, eaque, cumque totam velit aspernatur minus iure rem iusto laudantium illo ullam, reiciendis alias libero aliquam?Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div>
                 <div class="depoimento-single">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident pariatur omnis nam eius nihil, eaque, cumque totam velit aspernatur minus iure rem iusto laudantium illo ullam, reiciendis alias libero aliquam?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, ducimus rerum totam, voluptates nam hic sequi libero soluta saepe harum adipisci reprehenderit dolor incidunt quibusdam tempora tempore aut consequuntur cupiditate?</p>
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident pariatur omnis nam eius nihil, eaque, cumque totam velit aspernatur minus iure rem iusto laudantium illo ullam, reiciendis alias libero aliquam?Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div>
                 <div class="depoimento-single">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident pariatur omnis nam eius nihil, eaque, cumque totam velit aspernatur minus iure rem iusto laudantium illo ullam, reiciendis alias libero aliquam?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, ducimus rerum totam, voluptates nam hic sequi libero soluta saepe harum adipisci reprehenderit dolor incidunt quibusdam tempora tempore aut consequuntur cupiditate?</p>
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident pariatur omnis nam eius nihil, eaque, cumque totam velit aspernatur minus iure rem iusto laudantium illo ullam, reiciendis alias libero aliquam?Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                     <p class="nome-autor">Lorem Ipsum</p>
                 </div>
             </div>
-            <div class="w50 left">
+            <div class="w50 left servicos-container">
                 <h2 class="title">Serviços</h2>
                 <ul>
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae id necessitatibus aliquam rerum officiis inventore ducimus unde nihil tenetur minima autem ipsum animi, obcaecati esse quisquam! Iure nesciunt ad earum.</li>
@@ -112,7 +115,7 @@
 
     <footer>
         <div class="center">
-            <p>Todos os direitos reservados</p>
+            <p>Paulo Victor - Todos os direitos reservados</p>
         </div>
     </footer>
 
