6c8a790c47f262b8c5c3766f18dfb981643f6099
Paulo Victor de Souza Santos <50722782+PauloV231@users.noreply.github.com>
2020-07-31 15:25:30 -0300

    doctype html

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang='pt-br'>
- <head>
+<head>
     <title>Projeto 01</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
